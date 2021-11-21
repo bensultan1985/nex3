1e5d6a59fdd6065a00adf44409773e9a93f55c92
bensultan1985 <bensultan1985@gmail.com>
2021-11-20 21:04:48 -0800

    started fixing button styles

--- DelEvent.js
+++ DelEvent.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {View, SafeAreaView, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native'
 // import Icon from 'react-native-vector-icons/Fontisto';
 // import { DeleteOutlined } from 'ant-design/icons'
+import {styles} from './styles.js';
 
 
  const DelEvent = ({text, func, mainText, item, SetData}) => {
@@ -15,7 +16,7 @@ import {View, SafeAreaView, Text, StyleSheet, FlatList, TouchableOpacity} from '
   return (
     <TouchableOpacity onPress={() => {delEvent(item)}} style={{alignSelf: 'flex-end', flex: 1}}>
         <View style={styles.button}>
-            <Text style={styles.buttonText}>
+            <Text style={[styles.delButtonText,styles.buttonText]}>
                 remove
                 {/* <Icon name="arrow" size={30} color="#900" /> */}
             </Text>
@@ -23,32 +24,6 @@ import {View, SafeAreaView, Text, StyleSheet, FlatList, TouchableOpacity} from '
     </TouchableOpacity>
   );
  }
-
- const styles = StyleSheet.create({
-     button: {
-      //  borderRadius: 21,
-       borderWidth: 2,
-       fontSize: 20,
-      //  alignSelf: 'flex-start',
-      //  alignSelf: 'flex-end',
-       padding: 0,
-    //    paddingLeft: 6,
-    //    paddingRight: 6,
-      //  margin: 4,
-       borderBottomWidth: 0,
-       borderRightWidth: 0,
-      //  width: "50%",
-       borderColor: 'gray',
-   },
-   buttonText: {
-    fontSize: 20,
-    alignSelf: 'center',
-    padding: 1,
-    paddingLeft: 10,
-    paddingRight: 10,
-    color: 'gray'
-   }
- })
  
  export default DelEvent;
  
---
+++ styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+
+     button: {
+      //  borderRadius: 21,
+       borderWidth: 2,
+       fontSize: 20,
+      //  alignSelf: 'flex-start',
+      //  alignSelf: 'flex-end',
+       padding: 0,
+    //    paddingLeft: 6,
+    //    paddingRight: 6,
+    //    margin: 4,
+       borderBottomWidth: 0,
+       borderRightWidth: 0,
+       borderLeftWidth: 0,
+      //  width: "50%",
+       borderColor: 'gray',
+   },
+   editButtonText: {
+    fontSize: 20,
+    alignSelf: 'center',
+    padding: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'gray'
+   },
+   delButtonText: {
+    fontSize: 20,
+    alignSelf: 'center',
+    padding: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'gray'
+   },
+   completeButtonText: {
+    fontSize: 20,
+    alignSelf: 'center',
+    padding: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: 'gray'
+   }
+
+
+
+})
+
+// export default styles;
